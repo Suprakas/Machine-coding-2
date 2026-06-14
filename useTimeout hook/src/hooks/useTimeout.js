5f3ea44f2f6b26eeeb73a4d62d33230b5c926ece
SUPRAKASH BALLAV <Suprakashballav1997@gmail.com>
2026-06-14 15:13:06 +0530

    useTimeout hook implemented

---
+++ useTimeout hook/src/hooks/useTimeout.js
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from "react";
+
+export default function useTimeout(callback, delay) {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    if (delay == null) return;
+
+    const id = setTimeout(() => {
+      savedCallback.current?.();
+    }, delay);
+    return () => {
+      clearTimeout(id);
+    };
+  }, [delay]);
+}
